3a8e6886a982f624cf9c6e2eeafb8c9a3ebd70ce
Ryan Skidmore <ryan.skidmore@yahoo.com>
2023-02-28 22:19:34 +0000

    Mock data schema

--- config/bundles.php
+++ config/bundles.php
@@ -3,5 +3,4 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
