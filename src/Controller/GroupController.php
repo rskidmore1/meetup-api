22142022080e87c81baac2236b762e97218caef1
Ryan Skidmore <ryan.skidmore@yahoo.com>
2023-03-17 17:55:53 +0000

    create-group POST

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use MongoDB\BSON\ObjectId;
@@ -34,4 +35,69 @@ class GroupController extends AbstractController
         );
 
     }
+
+
+//  #[Route('/comment/save-group', name: 'group', methods: ['PUT'])] // here: todo: add this route to routes.yaml
+//     public function saveGroup(DocumentManager $dm, Request $request): Response
+//     {
+
+//         $parameters = json_decode($request->getContent(), true);
+
+//         if (! $parameters) {
+//             throw $this->parameterNotFoundException('No parameter found');
+//         }
+//         $group = $dm->getRespository(Group::class)->findAndUpdate()
+//             ->field
+
+//         $group = new Group();
+//         $comment->setUser(
+//             [
+//                 'id' => $parameters['user']['id'],
+//                 'name' => $parameters['user']['name'],
+//                 'photo' => $parameters['user']['photo']
+//             ]
+//         );
+//         $comment->setText($parameters['text']);
+//         $comment->setTopLevelComment($parameters['top_level_comment']);
+//         $comment->setReplies($parameters['replies']);
+//         $comment->setParentObjectId($parameters['parent_object_id']);
+//         $comment->setParentCommentId($parameters['parent_comment_id']);
+
+//         $dm->persist($comment);
+//         $dm->flush();
+
+//         return new Response(
+//               json_encode(['status' => 'entered']),
+//               Response::HTTP_OK,
+//               ['content-type' => 'application/json']
+//         );
+//     }
+
+
+ #[Route('/group/create-group', name: 'create_group', methods: ['POST'])] // here: todo: add this route to routes.yaml
+    public function createGroup(DocumentManager $dm, Request $request): Response
+    {
+
+        $parameters = json_decode($request->getContent(), true);
+
+        if (! $parameters) {
+            throw $this->parameterNotFoundException('No parameter found');
+        }
+
+        $group = new Group(); // Should i name these vars generically so i cna just copy paste them and change less vars?
+        $group->setName($parameters['name']);
+        $group->setPicture($parameters['picture']);
+        $group->setDescription($parameters['description']);
+        $group->setLocation($parameters['location']);
+
+        $dm->persist($group);
+        $dm->flush();
+
+        return new Response(
+              json_encode(['status' => 'entered']),
+              Response::HTTP_OK,
+              ['content-type' => 'application/json']
+        );
+    }
+
 }
