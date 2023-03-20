7d04686aa26a2acbe1d40267b2fe4afc24c39980
Ryan Skidmore <ryan.skidmore@yahoo.com>
2023-03-20 22:35:43 +0000

    UserController

---
+++ src/Controller/UserController.php
@@ -0,0 +1,64 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use MongoDB\BSON\ObjectId;
+
+use App\Document\User;
+use Doctrine\ODM\MongoDB\DocumentManager;
+
+class UserController extends AbstractController
+{
+    #[Route('/user/retrieve-user/{id}', name: 'retrieve_user')]
+    public function retrieveUser(DocumentManager $dm, $id): Response
+    {
+        $someId = new ObjectId($id);
+
+        $user = $dm->getRepository(User::class)->findBy(["_id" => $someId]);
+
+        if (! $user) {
+            throw $this->createNotFoundException('No user found for id ' . $id);
+        }
+
+        return new Response(
+              json_encode(['user' => $user]),
+              Response::HTTP_OK,
+              ['content-type' => 'application/json']
+        );
+    }
+
+    #[Route('/user/save-user', name: 'save_user', methods: ['POST'])] // here: todo: add this route to routes.yaml
+    public function saveUser(DocumentManager $dm, Request $request): Response
+    {
+
+        $parameters = json_decode($request->getContent(), true);
+
+        if (! $parameters) {
+            throw $this->parameterNotFoundException('No parameter found');
+        }
+
+        $user = new User();
+
+        $user->setName($parameters['name']);
+        $user->setLocation($parameters['location']);
+        $user->setJoinedDate($parameters['joined_date']);
+        $user->setLastVisitedDate($parameters['last_visited_date']);
+        $user->setAge($parameters['age']);
+        $user->setIntroduction($parameters['introduction']);
+        $user->setActivityInterests($parameters['activity_interests']);
+        $user->setGroups($parameters['groups']);
+
+
+        $dm->persist($user);
+        $dm->flush();
+
+        return new Response(
+              json_encode(['status' => 'entered']),
+              Response::HTTP_OK,
+              ['content-type' => 'application/json']
+        );
+    }
+}
